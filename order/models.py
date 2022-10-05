b6bbfa54af731c3b2faddeff7777d73ce7a47d02
mayank <mayank.infograins@gmail.com>
2022-10-05 15:33:20 +0530

    working......

--- order/models.py
+++ order/models.py
@@ -6,8 +6,10 @@ from product.models import (
 
 # Create your models here.
 
-# class Order(models.Model):
-#     buyer = models.ForeignKey(User,on_delete=models.CASCADE)
-
-#     pass
+class Order(models.Model):
+    # order_id = models.
+    transaction_id = models.CharField(max_length=256)
+    buyer = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    pass
 
